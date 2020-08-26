b629e7df2ed474406fd4b248455c56f72ad6ccde
teamadmino <teamadmino@icloud.com>
2020-08-26 13:13:52 -0700

    Main class prototype

---
+++ v2newapp/Main.java
@@ -0,0 +1,26 @@
+package com.teamadmino.application.main;
+
+import static com.teamadmino.admino_backend.server.database.ServerMain.workdir;
+
+import ch.qos.logback.classic.util.ContextInitializer;
+import com.teamadmino.admino_backend.TeamAdminoBackend;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class Main {
+
+    public static Logger log = null;
+
+    public static void main(String[] args) {
+
+        new File(workdir).mkdir();
+        System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, "config/logconfig.xml");
+        log = LoggerFactory.getLogger(Main.class);
+        log.info("Starting application...");
+        TeamAdminoBackend.start(args, log);
+
+    }
+
+}
