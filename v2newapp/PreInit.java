737cdcaff9690689db2a6a56c2b0b2b35d50e512
teamadmino <teamadmino@icloud.com>
2021-10-06 13:29:22 -0700

    new app resources

---
+++ v2newapp/PreInit.java
@@ -0,0 +1,11 @@
+package com.teamadmino.ARTIFACT.main;
+
+import static com.teamadmino.admino_backend.server.database.ServerMain.*;
+
+public class PreInit {
+
+    public static void main(String[] args) {
+        log.info("Custom PreInit methods");
+    }
+
+}
