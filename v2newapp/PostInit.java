8d540b359f0d87add775221a6d6d01785c3af815
teamadmino <teamadmino@icloud.com>
2021-12-03 16:02:51 -0800

    postinit skeleton

---
+++ v2newapp/PostInit.java
@@ -0,0 +1,11 @@
+package com.teamadmino.ARTIFACT.main;
+
+import static com.teamadmino.admino_backend.server.database.ServerMain.*;
+
+public class PostInit {
+
+    public static void main(String[] args) {
+        log.info("Custom PostInit methods");
+    }
+
+}
